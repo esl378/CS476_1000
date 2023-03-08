e896aaf6b559bf194f044529d2ea67232298a4e7
James <jameskress78@gmail.com>
2023-03-08 16:48:41 -0600

    initial

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-import server from './node-starter/config/server';
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => {
-    console.log('app running on port ${PORT}');
-});
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 4111;
+app.listen(PORT, console.log("Server don start for port: " + PORT))
