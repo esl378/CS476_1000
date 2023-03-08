76522252fe45223187f47eb7e9ba1542f76aee7c
James <jameskress78@gmail.com>
2023-03-08 17:09:56 -0600

    added some more things Co-authored-by: esl378 <esl378@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 4111;
 app.listen(PORT, console.log("Server don start for port: " + PORT))
+
+const mongoose = require('mongoose');
+const dotenv = require("dotenv");
+dotenv.config();
+
+//mongoDB connection
+const database = process.env.MONGOLAB_URI;
+mongoose.connect(database, {useUnifiedTopology: true, useNewUrlParser: true})
+.then(() => console.log('e don connect'))
+.catch(err => console.log(err));
+
+
+app.set('view engine', 'ejs');
+
+//Routes
+app.use('/', require('./routes/login'));
+const PORT = process.env.PORT || 4111;
+app.listen(PORT,console.log("Server has started at port " + PORT)); 
