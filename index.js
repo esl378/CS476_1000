f7260132c04ea54dc0dafd6338da4148287fabb1
James <jameskress78@gmail.com>
2023-03-08 22:16:00 -0600

    temporary delete from DB function

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 const express = require('express');
 const app = express();
-app.listen(PORT, console.log("Server don start for port: " + PORT))
 
-const mongoose = require('mongoose');
+
+
 const dotenv = require("dotenv");
 dotenv.config();
 
-//mongoDB connection
-const database = process.env.MONGOLAB_URI;
-mongoose.connect(database, {useUnifiedTopology: true, useNewUrlParser: true})
-.then(() => console.log('e don connect'))
-.catch(err => console.log(err));
+
 
 
 app.set('view engine', 'ejs');
