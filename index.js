b9909026c7762f0a0ec7a4c0bb5d4a830ae75b04
esl378 <ericlindemann123@gmail.com>
2023-03-09 14:45:23 -0600

    Added a routes file to allow the separation of routing from controllers

--- index.js
+++ index.js
@@ -17,50 +17,11 @@ app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
 //serve static files
-app.use(express.static(path.join(__dirname, '/public')))
+app.use(express.static(path.join(__dirname, '/public')));
 
-/*
+app.use('/', require('./routes/root'));
 
-Template for adding new pages that are servable from the webserver
 
-Replace the fileName with the name of your webpage without touching anything else
-
-app.get('fileName(.html)?', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'fileName.html'));
-});
-
-*/
-
-//Serve a page, lacking images and other files
-//semesterView page and will be treated as index page
-app.get('^/$|index(.html)?|semesterView(.html)', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'semesterView.html'));
-});
-
-//monthView page
-app.get('/monthView(.html)?', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'monthView.html'));
-});
-
-//delete page
-app.get('/delete(.html)?', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'delete.html'));
-});
-
-//deleteConfirm page
-app.get('/deleteConfirm(.html)?', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'deleteConfirm.html'));
-});
-
-//add page
-app.get('/add(.html)?', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'add.html'));
-});
-
-//edit page
-app.get('/edit(.html)?', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'edit.html'));
-});
 
 //I commented out this code just in case it messes with the page retrievals
 //The web server should now serve our html pages
---
+++ routes/root.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+
+/*
+
+Template for adding new pages that are servable from the webserver
+
+Replace the fileName with the name of your webpage without touching anything else
+
+router.get('fileName(.html)?', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'fileName.html'));
+});
+
+*/
+
+//Serve a page, lacking images and other files
+//semesterView page and will be treated as index page
+router.get('^/$|index(.html)?|semesterView(.html)', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'views', 'semesterView.html'));
+});
+
+//monthView page
+router.get('/monthView(.html)?', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'views', 'monthView.html'));
+});
+
+//delete page
+router.get('/delete(.html)?', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'views', 'delete.html'));
+});
+
+//deleteConfirm page
+router.get('/deleteConfirm(.html)?', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'views', 'deleteConfirm.html'));
+});
+
+//add page
+router.get('/add(.html)?', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'views', 'add.html'));
+});
+
+//edit page
+router.get('/edit(.html)?', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'views', 'edit.html'));
+});
+
+module.exports = router;
