7b3ca7a40144d33f06bce601724d65ca7beb631f
esl378 <ericlindemann123@gmail.com>
2023-03-09 12:33:42 -0600

    Didn't change anything

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
 //serve static files
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, '/public')))
 
 /*
 
