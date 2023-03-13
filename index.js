540904b3f007feb10f9b3dfdafeef10d6d2fbc15
esl378 <ericlindemann123@gmail.com>
2023-03-13 17:09:19 -0600

    did stuff

--- index.js
+++ index.js
@@ -44,6 +44,7 @@ app.use('/auth', require('./routes/auth'));
 app.use('/register', require('./routes/register'));
 app.use('/refresh', require('./routes/refresh'));
 app.use('/logout', require('./routes/logout'));
+app.use('/add', require('./routes/add'))
 
 
 app.use(verifyjwt);
