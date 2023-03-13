8ddd56d008b70ee9e4da7da38f6fdac6d4e2f511
ljcookson <ljc806@uregina.ca>
2023-03-13 17:09:29 -0600

    Add JS pages for Semester and Month Views

--- index.js
+++ index.js
@@ -44,6 +44,8 @@ app.use('/auth', require('./routes/auth'));
 app.use('/register', require('./routes/register'));
 app.use('/refresh', require('./routes/refresh'));
 app.use('/logout', require('./routes/logout'));
+//app.use('/monthRoute', require('./routes/monthRoute'));
+//app.use('/semesterRoute', require('./routes/semesterRoute'));
 
 
 app.use(verifyjwt);
