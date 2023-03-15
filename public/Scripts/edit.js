e43521546a8324b11ff5d508dcc7fd1213340768
esl378 <ericlindemann123@gmail.com>
2023-03-14 21:58:30 -0600

    Did stuff

--- public/Scripts/edit.js
+++ public/Scripts/edit.js
@@ -1,3 +1,8 @@
+//
+//                                  Classes
+//
+
+
 //Created a class to get the semesters into a nice easy to use object
 class Semester {
     constructor(name,heldIn,strtDate,endDate,year) {
@@ -71,12 +76,21 @@ class DTO {
     }
 }
 
+//
+//                                        Variables
+//
+
 var semHidden = false;
 var dateHidden = false;
 
 var semesters = new Array();
 var events = new Array();
 var years = new Array();
+
+
+//
+//                                        Helper functions
+//
 
 //Hide semesters tab
 function hideSemesters() {
@@ -118,6 +132,11 @@ function hideDates() {
         dateHidden = !dateHidden;
     }
 }
+
+
+//
+//                        For adding HTML to the page
+//
 
 //Adds a new semester object to the form
 function createSemesterObject() {
@@ -188,6 +207,55 @@ function createSemesterObject() {
 
 }
 
+//Adds a new semester object to the form
+function createSemesterObject(description, strtDate, endDate, year, heldIn) {
+
+    //Get the semesters div
+    var semesters = document.getElementById("semesters");
+    var num = semesters.childNodes.length + 1;
+
+    //Create outer div
+    var div = document.createElement("div");
+    div.className = "semester";
+
+    //Title
+    var name = document.createElement("h3");
+    name.innerHTML = "Semester " + num;
+    div.appendChild(name);
+
+    //Description
+    var name = document.createElement("p");
+    name.innerHTML = "Description: " + description;
+    //Add name to div
+    div.appendChild(name);
+
+
+    //Start date
+    var name = document.createElement("p");
+    name.innerHTML = "Start date: " + strtDate;
+    div.appendChild(name);
+
+    //End date
+    var name = document.createElement("p");
+    name.innerHTML = "End date: " + endDate;
+    div.appendChild(name);
+
+    //year
+    var name = document.createElement("p");
+    name.innerHTML = "Year: " + year;
+    div.appendChild(name);
+
+    //Held in 
+    var name = document.createElement("p");
+    name.innerHTML = "Held in: " + heldIn;
+    div.appendChild(name);
+
+
+    //add div to semesters div
+    semesters.appendChild(div);
+
+}
+
 //Adds a new date object
 function createDateObject() {
 
@@ -253,12 +321,76 @@ function createDateObject() {
     dates.appendChild(div);
 }
 
+//Adds a new date object
+function createDateObject(description, strtDate, endDate, year, semester) {
+
+    //Get the form to add a new object
+        var dates = document.getElementById("dates");
+        var num = dates.childNodes.length + 1;
+        
+    //Create the outer div
+        var div = document.createElement("div");
+        div.className = "date";
+    
+        //Title
+        var name = document.createElement("h3");
+        name.innerHTML = "Date " + num;
+        div.appendChild(name);
+    
+        //Add the inputs to form
+        //Description
+        name = document.createElement("p");
+        name.innerHTML = "Description: " + description;
+        //Add name to div
+        div.appendChild(name);
+    
+        //Start date
+        name = document.createElement("p");
+        name.innerHTML = "Start date: " + strtDate;
+        //Add name to div
+        div.appendChild(name);
+    
+        //End date
+        name = document.createElement("p");
+        name.innerHTML = "End date: " + endDate;
+        //Add name to div
+        div.appendChild(name);
+
+        //Year
+        name = document.createElement("p");
+        name.innerHTML = "Year: " + year;
+        //Add name to div
+        div.appendChild(name);
+
+        //Semester
+        name = document.createElement("p");
+        name.innerHTML = "Semester: " + semester;
+        //Add name to div
+        div.appendChild(name);
+    
+        //Append the div to the form
+        dates.appendChild(div);
+    }
+
+    //Creates a new Year object
+    function createYearObject(year) {
+        var opt = document.getElementById("year");
+
+        var name = document.createElement("option");
+        name.value = year;
+        name.innerHTML = year;
+    }
+
+//
+//                              This is the function that does the object stuff
+//
+
 async function mommyFunky() {
     let jsonSemesters;
     let jsonYears;
     let jsonEvents;
-    //Getting semester data
-    try{
+    //-------------------------------Getting semester data--------------------------------------------------------------------------------
+        try{
         const result = await fetch('http://localhost:4111/getSemesters', {
             method:'POST',
             headers: {
@@ -280,7 +412,7 @@ async function mommyFunky() {
         console.log("Couldn't get the semesters");
     }
     
-    //Getting Event data
+    //------------------------Getting Event data----------------------------------------------------------------------------------------------
     try{
         const result = await fetch('http://localhost:4111/getEvents', {
             method:'POST',
@@ -303,7 +435,7 @@ async function mommyFunky() {
         console.log("Couldn't get the events");
     }
 
-    //Getting the Year data
+    //----------------------Getting the Year data-------------------------------------------------------------------------------------------------
     try{
         const result = await fetch('http://localhost:4111/getYears', {
             method:'POST',
@@ -328,10 +460,9 @@ async function mommyFunky() {
 
     for(let i = 0; i < jsonYears.length; i++) {
         let tmpYear = new Year(jsonYears[i].year, jsonYears[i]._id);
-        console.log(tmpYear);
         years.push(tmpYear);
     } 
-
+    //name,heldIn,strtDate,endDate,year
     for(let i = 0; i < jsonSemesters.length; i++) {
         let tmpSem = new Semester(
             jsonSemesters[i].name, 
@@ -339,22 +470,40 @@ async function mommyFunky() {
             jsonSemesters[i].start_date, 
             jsonSemesters[i].end_date, 
             jsonSemesters[i].year,
-            jsonSemesters[i]._id);
+            jsonSemesters[i]._id
+            );
 
         semesters.push(tmpSem);
     }  
-
+    
+    //strtDate, endDate, semester="invalid", year, description, id
     for(let i = 0; i < jsonEvents.length; i++) {
         let tmpEven = new Event(
-            jsonEvents[i].description,
             jsonEvents[i].strtDate,
             jsonEvents[i].endDate,
+            jsonEvents[i].semester,
             jsonEvents[i].year,
             jsonEvents[i].description,
             jsonEvents[i]._id
-        );
+            );
         events.push(tmpEven);
     } 
 
-    console.log(events);
+    for(let i = 0; i < events.length; i++) {
+        createDateObject(
+            events[i].description, 
+            events[i].strtDate, 
+            events[i].endDate, 
+            events[i].year, 
+            events[i].semester);
+    }
+
+    for(let i = 0; i < semesters.length; i++) {
+        createSemesterObject(
+            semesters[i].name, 
+            semesters[i].strtDate, 
+            semesters[i].endDate, 
+            semesters[i].year, 
+            semesters[i].heldIn);
+    }
 }
