a69e103f90e976d358052d75e529d65dd58bd359
James <60630102+Jkress78@users.noreply.github.com>
2023-03-20 15:49:34 -0600

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,9 @@
 const allowedOrigins = [
     'https://www.google.ca',
     'http://127.0.0.1:5500',
-    'http://localhost:4111'
+    'http://localhost:4111',
+    'http://jfdklfjdf.azurewebsites.net',
+    'http://azurewebsites.net'
 ];
 
 module.exports = allowedOrigins;
